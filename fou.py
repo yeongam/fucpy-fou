6f803a2435b24c34fe9b25c2dfaa199c7670748c
yeongam <82902123+yeongam@users.noreply.github.com>
2022-10-24 19:34:12 +0900

    Add files via upload

---
+++ fou.py
@@ -0,0 +1,5 @@
+def print_something(my_name, your_name = "Computer"):
+    print("Hello {0}, My name is{1}".format(your_name, my_name))
+
+print_something("Yeongam", "Computer")
+print_something("Yeongam")
